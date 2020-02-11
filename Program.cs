223c93a05f853ccb575d3dabfd9884fbb2c960b4
Adrian Abreu <aabreuglez@gmail.com>
2020-02-11 22:56:30 +0100

    Not working proposal

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+namespace console_app_logging_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new ServiceCollection();
+            collection.AddLogging(builder => {
+                builder
+                .AddConsole()
+                .AddApplicationInsights("key")
+                .SetMinimumLevel(LogLevel.Information);
+            });
+            var provider = collection.BuildServiceProvider();
+
+            var logger = provider.GetService<ILogger<Program>>();
+            logger.LogInformation("Testing");
+        }
+    }
+}
